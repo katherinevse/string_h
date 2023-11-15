a2aea1132336dad7f5678163b8f1c92cf516d221
Loris Pamula <lorispam@il-k5.msk.21-school.ru>
2023-11-15 11:57:57 +0300

    -

--- src/s21_memcpy.c
+++ src/s21_memcpy.c
@@ -7,3 +7,29 @@ void *s21_memcpy(void *dest, const void *src, s21_size_t n) {
   }
   return dest;
 }
+
+// void *s21_memcpy(void *dest, const void *src, s21_size_t n) {
+//   char *char_dest = (char *)dest;
+//   const char *char_src = (const char *)src;
+
+//   for (s21_size_t i = 0; i < n; i++) {
+//     char_dest[i] = char_src[i];
+//   }
+
+//   return dest;
+// }
+
+// #include <stdio.h>
+
+// int main() {
+//   char source[] = "Hello, World!";
+//   char destination[20];
+
+//   s21_memcpy(destination, source, 13); 
+
+//   // Выводим результат
+//   printf("Source: %s\n", source);
+//   printf("Destination: %s\n", destination);
+
+//   return 0;
+// }
