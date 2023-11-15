9c338f5620ba114d782d0a6d838cddf1f238fed6
Katherine <katherinevse@mail.ru>
2023-11-15 21:59:14 +0300

    -

---
+++ src/s21_sprintf.c
@@ -0,0 +1,20 @@
+
+
+
+typedef struct {
+    int minus; //к левому краю
+    int plus; //знак
+    int space_plus; //+символ под знак
+    int hash;
+    int zero;
+    int width;
+    int accuracy;
+    char length;
+    int number_system;
+    int flag_size;
+    int dot;
+    int upper_case;
+    int g;
+    int e;
+} Spec;
+
