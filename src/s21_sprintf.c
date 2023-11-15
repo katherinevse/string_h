baecad944c615cbc7419063871c11a73ab704761
Katherine <katherinevse@mail.ru>
2023-11-15 22:01:38 +0300

    -

--- src/s21_sprintf.c
+++ src/s21_sprintf.c
@@ -6,10 +6,10 @@ typedef struct {
     int plus; //знак
     int space_plus; //+символ под знак
     int hash; // вывод 16 и 8 ричных чисел в формате 0х и 0 соответсвенно
-    int zero;
-    int width;
-    int accuracy;
-    char length;
+    int full_zero; // заполнить нулями а не пробелами
+    int width; // ширина после точки я так понимаю
+    int accuracy; // точность
+    char length; //тип переменной ld,lf, Lf и тд
     int number_system;
     int flag_size;
     int dot;
