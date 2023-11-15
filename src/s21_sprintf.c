a0e553156cf906944424da1150e1d76e849a6000
Katherine <katherinevse@mail.ru>
2023-11-15 21:59:51 +0300

    -

--- src/s21_sprintf.c
+++ src/s21_sprintf.c
@@ -5,7 +5,7 @@ typedef struct {
     int minus; //к левому краю
     int plus; //знак
     int space_plus; //+символ под знак
-    int hash;
+    int hash; // вывод 16 и 8 ричных чисел в формате 0х и 0 соответсвенно
     int zero;
     int width;
     int accuracy;
