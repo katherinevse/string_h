0b57fb3eebc9cf1c16c058ff53b5334075859d55
Loris Pamula <lorispam@il-k5.msk.21-school.ru>
2023-11-15 12:15:36 +0300

    -

--- src/s21_memset.c
+++ src/s21_memset.c
@@ -8,6 +8,24 @@ void *s21_memset(void *str, int c, s21_size_t n) {
     return str;
 }
 
+// void *s21_memset(void *str, int c, s21_size_t n){
+//     char *str_str = (char*)str;
+//     for (s21_size_t i = 0; i < n; i++)
+//     {
+//         str_str[i]= (char)c;
+//     }
+// }
 
+// int main() {
+//     char buffer[10]; 
 
+//     s21_memset(buffer, 'A', 10); 
 
+//     // Выводим результат
+//     for (int i = 0; i < 10; i++) {
+//         printf("%c ", buffer[i]);
+//     }
+//     printf("\n");
+
+//     return 0;
+// }
