c5f4a37e0b926b71f24080aab5705482a9200934
Katherine <katherinevse@mail.ru>
2023-11-16 12:38:35 +0300

    -

--- src/s21_sprintf.c
+++ src/s21_sprintf.c
@@ -1,3 +1,4 @@
+#include<stdarg.h>
 
 
 
@@ -7,7 +8,7 @@ typedef struct {
     int space_plus; //+символ под знак
     int hash; // вывод 16 и 8 ричных чисел в формате 0х и 0 соответсвенно
     int full_zero; // заполнить нулями а не пробелами
-    int width; // ширина после точки я так понимаю
+    int width; // ширина после точки
     int accuracy; // точность
     char length; //тип переменной ld,lf, Lf и тд
     int number_system; // система начисления
@@ -22,4 +23,9 @@ typedef struct {
 int s21_sprintf(char *str, const char *format, ...){
     char specifiers[] = "dfsgGeExXcuiopn";
     сhar *src = str; //изначальное положение str записываем в src чтобы узнать кол-во записанных символов
+    va_list arguments; // считывание ...
+
+
+
+    return(str-src); //возвращаем кол-во записанных символов
 }
