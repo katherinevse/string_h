1b0f861acf5001bac4c110ad2c505d241b518ef2
Katherine <katherinevse@mail.ru>
2023-11-26 22:49:48 +0300

    -

--- src/s21_sprintf.c
+++ src/s21_sprintf.c
@@ -102,6 +102,16 @@ const char *set_specs(Spec *specs, const char *format, va_list *arguments) {
     return format;
 }
 
+//установка шестнад. сист
+Spec place_number_system(Spec specs, char format) {
+    if (format == 'o') specs.number_system = 8;
+    else if (format == 'x' || format == 'X') specs.number_system = 16;
+    if (format == 'X') specs.upper_case = 1;
+    return specs;
+}
+
+
+
 //СПЕЦИФИКАТОРЫ!
 char *read_spec(char *str, char *src, const char *format, Spec specs, va_list *arguments) {
   if (*format == 'd' || *format == 'i') {
@@ -262,21 +272,97 @@ char spec_print_u(char *str, Spec specs, char format, va_list *arguments) {
 }
 
 
+// int print_e(int e, s21_size_t *size_double, char *str_num, Spec specs, int *i) {
+//     int copy_e = e;
+//     if (copy_e == 0)
+//         *size_double -= add_sym_str(str_num + *i, i, get_num_char(copy_e % specs.number_system, specs.upper_case));
+//     while (copy_e) {
+//         *size_double -= add_sym_str(str_num + *i, i, get_num_char(copy_e % specs.number_system, specs.upper_case));
+//         copy_e /= 10;
+//     }
+// }
+
+
+
+
+
+
+char get_num_char(int num, int upper_case) {
+    char flag = '0';
+    switch (num) {
+        //if if has numerical sys more than 10...
+        case 10:
+            flag = (char)('a' - upper_case * 32);
+            break;
+        case 11:
+            flag = (char)('b' - upper_case * 32);
+            break;
+        case 12:
+            flag = (char)('c' - upper_case * 32);
+            break;
+        case 13:
+            flag = (char)('d' - upper_case * 32);
+            break;
+        case 14:
+            flag = (char)('e' - upper_case * 32);
+            break;
+        case 15:
+            flag = (char)('f' - upper_case * 32);
+            break;
+    }
+    //and less than 11
+    if (0 <= num && 9 >= num) flag = (char)(num + 48);
+    return flag;
+}
+
+
 //подсчет, сколько памяти под аргумент в str1
-int decimal_string(char *buf_str,Spec specs, unsigned long int num,s21_size_t size_num) {
-    int flag = 0,
-    i = 0; //для чего нам нжуен i???
-    if (specs.hash && specs.number_system == 8) specs.flag_size = 1;
-    //review of # spec and number-system
-    else if (specs.hash && specs.number_system == 16) specs.flag_size = 2;
+char print_decimal(char *str, Spec specs, va_list arguments) {
+    long int num = 0;
+    if (specs.length == 'l') {//по типу переменной разбираем текущий спецификатор (long, short, int)
+        num = (long int)va_arg(arguments, long int);
+    }
+    else if (specs.length == 'h') {
+        num = (short)va_arg(arguments, short);
+    }
+    else {
+        num = (int)va_arg(arguments, int);
+    }
+    s21_size_t size_decimal = get_size_decimal(&specs, num);
+    //вызов функции для подсчета буфера для числа (последовательное деление на 10)
+    char *str_num = malloc(sizeof(char) * size_decimal);
+    //allocating memory for string with size of result we had of integer if size_decimal
+    if (str_num)//placing symbols in reverse order in str_num
+    {
+        int i = decimal_string(specs, num, str_num, size_decimal);      //ОБРАТИТЬ ВНИМАНИЕ 
+        //reversing buffer string to majot str
+        for (int j = i - 1; j >= 0; j--) {
+            *str = str_num[j];
+            str++;
+        }
+        while (i < specs.width) {
+            *str = ' ';
+            str++;
+            i++;//i am not sure about this string (188), but i think without it we will have eternal cycle
+        }
+    }
+    if (str_num) free(str_num);
+    return str;
+}
+
+int decimal_string(Spec specs, long int num, char *str_num, s21_size_t size_decimal) {
+    int flag = 0, i = 0;
+    if (num < 0) {
+        num = -num;
+        flag = 1;
+    }
     long int copy_num = num;
-    i = decimal_string_zeros(specs, num, buf_str, size_num, i, copy_num, flag);
+    i = decimal_string_zeros(specs, num, str_num, size_decimal, i, copy_num, flag);
     //flags '+' '-' ' ' treatment
-    i = unsigned_decimal_string_helper(specs, num, buf_str, size_num, i);
+    i = decimal_string_helper(specs, num, str_num, size_decimal, i);
     return i;
 }
 
-//???
 int decimal_string_zeros(Spec specs, long int num, char *str_num, s21_size_t size_decimal, int i, int copy_num, int flag) {
     //printing number in a mass if it is 0
     if (((copy_num == 0) && (specs.accuracy || specs.width || specs.space)) || (copy_num == 0 && !specs.accuracy && !specs.width && !specs.space && !specs.dot)) {
@@ -315,6 +401,49 @@ int decimal_string_zeros(Spec specs, long int num, char *str_num, s21_size_t siz
     }
     return i;
 }
+
+int unsigned_decimal_string_helper(Spec specs, long int num, char *str_num, s21_size_t size_decimal, int i) {
+    if (specs.hash && specs.number_system == 8) {
+        str_num[i] = '0';
+        i++;
+        size_decimal--;
+    }
+    else if (specs.hash && specs.number_system == 16) {
+        if (specs.upper_case) str_num[i] = 'X';
+        else str_num[i] = 'x';
+        i++;
+        size_decimal--;
+        str_num[i] = '0';
+        i++;
+        size_decimal--;
+    }
+    return i;
+}
+
+s21_size_t get_size_decimal(Spec *specs,long int num) {
+    s21_size_t result = 0;//returning result
+    long int copy_num = num;//do not want to change num and break it
+    if (copy_num < 0) copy_num = -copy_num;//reroll from - num if it is less than 0
+    while (copy_num > 0) {
+        copy_num /= 10;
+        result++;//iterational changing of copy_num to count number of symbols in it
+    }
+    if (copy_num == 0 && result == 0 && (specs->accuracy || specs->width || specs->space)) result++;
+    //if we have one of these specs, we need to add one place to symbol even result = 0
+    if (result < (s21_size_t)specs->width) result = specs->width;
+    //if width is bigger we need to get a bigger result to make it equal to width
+    if (result < (s21_size_t)specs->accuracy) result = specs->accuracy;
+    //analogical to accuracy
+    if (specs->space || specs->plus || num < 0) {
+        specs->flag_size = 1;
+        result++;
+    }
+        //if we have spec to have additional one place to string or num have '-' in start of it we need to have additional place
+    if (result == 0 && copy_num == 0 && !specs->accuracy && !specs->width && !specs->space && !specs->dot) result++;
+    //if we still have result == 0 and other shit result should be 1 (for ex. we had a number "0")
+    return result;
+}
+
 
 
 
